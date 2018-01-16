c04556e1b5e62fb78e7370c42c639de6775d5a51
Kirill Khudyakov <hudiakov@gmail.com>
2018-01-16 19:47:48 +0300

    Add Services

---
+++ ImproveUp/Services/PrintService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ImproveUp.Services
+{
+    using Interfaces;
+
+    public class PrintService: IPrintableService {
+
+        public void print(IPatternInfo item) {
+             Console.WriteLine("" + item.name + "->" + item.description);            
+        }
+
+
+    }
+
+}
