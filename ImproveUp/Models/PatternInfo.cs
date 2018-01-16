cd8179be23f572535c7c28b7650f805a065da2ca
Kirill Khudyakov <hudiakov@gmail.com>
2018-01-16 19:47:21 +0300

    Add model PatternInfo

---
+++ ImproveUp/Models/PatternInfo.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace ImproveUp.Models
+{
+
+    using Interfaces;
+
+    public partial class PatternInfo : IPatternInfo {
+
+        public string name { get ;  }
+        public string description { get; }
+
+        public PatternInfo(string name) 
+        {
+            this.name = name;
+            this.description = "";
+        }
+
+        public PatternInfo((string name, string description) tuple)
+        {
+            this.name = tuple.name;
+            this.description = tuple.description;
+                
+        }
+
+    }
+
+    public partial class PatternInfo {
+
+        public void print(IPrintableService service) 
+        {
+            service.print(this);
+        }
+    }
+
+}
